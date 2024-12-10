056092c09b7a74ab3438e91ad2fd3c50a39b7bf4
Patrick Jose <patrickgjose@gmail.com>
2024-12-10 12:35:30 -0800

    Create player class

---
+++ src/Player.cpp
@@ -0,0 +1,40 @@
+#include "Player.h"
+
+using json = nlohmann::json;
+
+Player::Player(const std::string& spriteName) {
+    m_texture.loadFromFile(Global::spritesPath+spriteName+".png");
+    loadSpriteSheet(spriteName);
+
+    m_sprite.setTexture(m_texture);
+    m_sprite.setTextureRect({0,0,58,58});
+    m_sprite.setPosition(300.f, 200.f);
+}
+
+sf::Sprite& Player::getSprite() {
+    return m_sprite;
+}
+
+void Player::loadSpriteSheet(const std::string& spriteName) {
+    std::ifstream f(Global::spritesPath+spriteName+".json");
+    json sprite_sheet = json::parse(f);
+    for (const auto& sprite: sprite_sheet["sprites"]) {
+        std::string spriteName = sprite["fileName"];
+        spriteName = spriteName.substr(0,spriteName.find('.'));
+        int spriteNum = std::stoi(spriteName);
+        int width = sprite["width"];
+        int height = sprite["height"];
+        int x = sprite["x"];
+        int y = sprite["y"];
+        m_spriteSheetData[spriteNum-1] = {width,height,x,y};
+    }
+}
+
+void Player::updateSprite() {
+    m_currSprite = ((m_currSprite+1) % m_spriteSheetData.size());
+    int width = m_spriteSheetData[m_currSprite].width;
+    int height = m_spriteSheetData[m_currSprite].height;
+    int x = m_spriteSheetData[m_currSprite].x;
+    int y = m_spriteSheetData[m_currSprite].y;
+    m_sprite.setTextureRect({x,y,width,height});
+}
---
+++ src/Player.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <SFML/Graphics.hpp>
+#include "Global.h"
+#include "nlohmann/json.hpp"
+
+class Player {
+private:
+    struct SpriteSheet {
+        int width;
+        int height;
+        int x;
+        int y;
+    };
+
+    sf::Sprite m_sprite;
+    sf::Texture m_texture;
+    std::unordered_map<int,SpriteSheet> m_spriteSheetData;
+    int m_currSprite = 0;
+public:
+    Player(const std::string& textureName);
+    sf::Sprite& getSprite();
+    void loadSpriteSheet(const std::string& spriteName);
+    void updateSprite();
+};
