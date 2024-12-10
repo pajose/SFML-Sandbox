1e8844a7219b438e51dcd3a87def3a69c669d863
Patrick Jose <patrickgjose@gmail.com>
2024-12-10 12:35:07 -0800

    Move asset paths to global namespace

---
+++ src/Global.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace Global {
+    inline std::string texturesPath = "..\\..\\..\\assets\\textures\\";
+    inline std::string spritesPath = "..\\..\\..\\assets\\sprites\\";
+}
